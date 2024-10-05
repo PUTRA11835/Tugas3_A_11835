71a7dfa4958d51368608e20afec2ed2b155e456c
Putra Palampang Tarung <220711835@students.uajy.ac.id>
2024-10-05 15:12:31 +0700

    Update Tugas_Modul3_A_Gus_11835.py

--- Tugas_Modul3_A_Gus_11835.py
+++ Tugas_Modul3_A_Gus_11835.py
@@ -73,8 +73,8 @@ if uploaded_file is not None:
     st.write("<h3 style='text-align: center; color: #0073e6;'>Data yang diupload:</h3>", unsafe_allow_html=True)
     st.dataframe(input_data)
 
-    model_directory = r'D:\Semester 5\Kuliah\ML\Tugas3_A_11835'
-    model_path = os.path.join(model_directory, r'RR_IPK_model.pkl')
+
+    model_path = r'RR_IPK_model.pkl'
 
     if os.path.exists(model_path):
         with open(model_path, 'rb') as f:
